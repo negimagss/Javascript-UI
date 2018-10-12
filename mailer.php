373dce53e46ac92b3aef011361581acb816d3fd3
Shardul Negi <shardul.negi21@gmail.com>
2018-10-12 16:59:50 -0400

    yes

--- mailer.php
+++
@@ -1,9 +0,0 @@
-<?php
-$name = $_POST["name"];
-$to = "shardul.negi21@gmail.com";
-$subject = "[MyWebsite]" . $_POST["subject"];
-$txt = $_POST["body"];
-$headers = "From: Test Program for the same" . "\r\n";
-
-mail($to,$subject,$txt,$headers);
-?>
